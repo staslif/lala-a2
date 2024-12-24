45fc2055b9c255d83d6ae567ca60baea86b69110
staslif <149203375+staslif@users.noreply.github.com>
2024-12-24 10:53:51 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Diagnostics;
+
+const int multipleOfThisNumber = 14, maxArrayLength = 1000, maxValueInArray = 10000;
+ static void Main(string[] args)
+{
+    int[] array = new int[maxArrayLength]; 
+    Random r = new();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = r.Next(0, maxValueInArray);
+
+    int majorityElement = FindMajorityElement(array);
+
+    if (majorityElement != -1 && majorityElement % multipleOfThisNumber == 0)
+    {
+        Console.WriteLine($"{majorityElement} - это число соответствует условиям задачи.");
+    }
+    else
+    {
+        Console.WriteLine(-1);
+    }
+}
+ static int FindMajorityElement(int[] array)
+{
+    int candidate = 0;
+    int count = 0;
+    foreach (int num in array)
+    {
+        if (count == 0)
+        {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+    count = 0;
+    foreach (int num in array)
+    {
+        if (num == candidate)
+        {
+            count++;
+        }
+    }
+    return (count > array.Length / 2) ? candidate : -1;
+}
